756885dcd48b1563716462692d826df7a9e780c6
Emily Jirles <emily.jirles@gmail.com>
2018-07-11 21:00:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file.
 function lowerCaseDrivers(collection){
-  return collection.map(driver => driver.toLowercase());
+  return collection.map(driver => driver.toLowerCase());
 };
